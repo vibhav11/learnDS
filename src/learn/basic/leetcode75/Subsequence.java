5665263e5e5880854745fb4b5bae447087e9fac2
Vibhav <jain.vibhav017@gmail.com>
2023-05-09 23:45:06 +0530

    commit for isSubsequence problem

--- src/learn/basic/leetcode75/Subsequence.java
+++ src/learn/basic/leetcode75/Subsequence.java
@@ -2,17 +2,33 @@ package learn.basic.leetcode75;
 public class Subsequence {
     public boolean isSubsequence(String s, String t) {
         int startingPoint=0;
-        for (int i = 0; i < s.length(); i++) {
 
+        for (int i = 0; i < s.length(); i++) {
+            boolean match = false;
             for (int j = startingPoint; j < t.length(); j++) {
                 if(s.charAt(i)==t.charAt(j)){
                     startingPoint=j+1;
+                    match=true;
                     break;
-                }else {
-                    return false;
                 }
             }
+            if (match==false)
+                return false;
         }
         return true;
+    }
+
+    public boolean isSubsequenceTwoPointer(String s,String t){
+        int pointer1=0,pointer2=0;
+        while(pointer1<s.length() && pointer2<t.length()){
+            if (s.charAt(pointer1)==t.charAt(pointer2)){
+                pointer1++;
+                pointer2++;
+            }else{
+                pointer2++;
+            }
+        }
+        if (pointer1==s.length()) return true;
+        return false;
     }
 }
